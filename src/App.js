d6281a37a405c09b9eeeb32e82f45a8e15c3fd7f
rk8672 <88264158+rk8672@users.noreply.github.com>
2022-06-13 22:51:55 +0530

    Files upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React ,{useState}from 'react';
+import NewExpense from './components/NewExpense';
+import Expenses from './components/Expenses';
+
+
+
+let DUMMY_EXPENSE=[];
+
+const App=()=> {
+
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSE);  
+
+  const addExpenseHandler = (expense)=>{
+    const updatedExpense=[expense,...expenses];
+    setExpenses(updatedExpense);
+  }
+
+  return (
+    <div>
+        <NewExpense onAddExpense={addExpenseHandler} />
+        <Expenses item={expenses} />
+    </div>    
+);
+}
+
+export default App;
